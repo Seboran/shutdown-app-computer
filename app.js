5376257b168b1317e73e6079b562ca7e244c62b4
Seboran <nirinarabeson@outlook.fr>
2019-02-21 22:19:41 +0100

    changed main function to post

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import express from "express";
+import bodyParser from "body-parser";
 import wintools from "wintools";
 
 const app = express();
 
-app.get("/shutdown", function(req, res) {
-  wintools.shutdown.poweroff();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/shutdown", function(req, res) {
+  const delay = req.body.delay;
+  res.send("Received");
+  setTimeout(wintools.shutdown.poweroff(), delay * 60 * 1000);
 });
 
 app.listen(process.env.PORT || 3000);
